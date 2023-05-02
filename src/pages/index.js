e1fc20ccc8ad9ec94ac75f39e70e02d92ac6afdf
Edward Tay <edwardtay7@gmail.com>
2023-05-02 22:28:39 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,17 +7,17 @@ const ProfilePage = () => {
         <Image src="/profile-page/edwardtay.png" width={200} height={200} alt="Profile picture" />
         <br />
         <div className="profile-links">
-          <a href="https://www.linkedin.com/in/edwardtaychuern"><Image src="/linkedin.png" width={50} height={50} alt="linkedin logo" /></a>
+          <a href="https://www.linkedin.com/in/edwardtaychuern"><Image src="/profile-page/linkedin.png" width={50} height={50} alt="linkedin logo" /></a>
           <a href="https://github.com/edwardtay"><Image src="/profile-page/github.png" width={50} height={50} alt="github logo" /></a>
-          <a href="https://www.twitter.com/iamedwardtay"><Image src="/twitter.png" width={50} height={50} alt="twitter logo" /></a>
-          <a href="https://soundcloud.com/edwardtay"><Image src="/profile-page/public//soundcloud.png" width={50} height={50} alt="soundcloud logo" /></a>
-          <a href="https://quora.com/profile/Edward-Tay"><Image src="/quora.png" width={50} height={50} alt="quora logo" /></a>
+          <a href="https://www.twitter.com/iamedwardtay"><Image src="/profile-page/twitter.png" width={50} height={50} alt="twitter logo" /></a>
+          <a href="https://soundcloud.com/edwardtay"><Image src="/profile-page/soundcloud.png" width={50} height={50} alt="soundcloud logo" /></a>
+          <a href="https://quora.com/profile/Edward-Tay"><Image src="/profile-page/quora.png" width={50} height={50} alt="quora logo" /></a>
           <br />
-          <a href="https://oncyber.io/edwardtay"><Image src="/oncyber.png" width={50} height={50} alt="oncyber logo" /></a>
-          <a href="mailto:contact@edwardtay.com"><Image src="/email.png" width={50} height={50} alt="email logo" /></a>
-          <a href="https://medium.com/@edwardtay"><Image src="/medium.png" width={50} height={50} alt="medium logo" /></a>
-          <a href="https://www.lensfrens.xyz/edwardtay.lens"><Image src="/lens.png" width={50} height={50} alt="lens logo" /></a>
-          <a href="https://fcast.me/edwardtay"><Image src="/farcaster.png" width={50} height={50} alt="farcaster logo" /></a>
+          <a href="https://oncyber.io/edwardtay"><Image src="/profile-page/oncyber.png" width={50} height={50} alt="oncyber logo" /></a>
+          <a href="mailto:contact@edwardtay.com"><Image src="/profile-page/email.png" width={50} height={50} alt="email logo" /></a>
+          <a href="https://medium.com/@edwardtay"><Image src="/profile-page/medium.png" width={50} height={50} alt="medium logo" /></a>
+          <a href="https://www.lensfrens.xyz/edwardtay.lens"><Image src="/profile-page/lens.png" width={50} height={50} alt="lens logo" /></a>
+          <a href="https://fcast.me/edwardtay"><Image src="/profile-page/farcaster.png" width={50} height={50} alt="farcaster logo" /></a>
 
         </div>
       </div>
